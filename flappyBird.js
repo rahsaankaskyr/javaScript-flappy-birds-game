2e81a315f5e74b134bad92750521c924868cff5a
Rahsaan Basek <rahsaanbasek@gmail.com>
2017-12-31 13:10:13 -0500

    javascript functions for environment instructions.txt

--- flappyBird.js
+++ flappyBird.js
@@ -3,4 +3,11 @@ function draw(){
 	ctx.drawImage(bg, 0, 0);
 	ctx.drawImage(pipeNorth, pX, pY);
 	ctx.drawImage(pipeSouth, pX, pY+Const);
+
+
+	ctx.drawImage(fg, 0, cvs.height - fg.height);
+
+	ctx.drawImage(bird, bX, bY);
+
+
 }
