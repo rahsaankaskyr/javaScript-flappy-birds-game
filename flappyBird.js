7bf025a4d33951a599059c6f73a7c45ed09fdb9e
Rahsaan Basek <rahsaanbasek@gmail.com>
2017-12-31 16:39:11 -0500

    js file load images

--- flappyBird.js
+++ flappyBird.js
@@ -1,3 +1,16 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+// load images
+
+var bird; = new Image();
+var bg; = new Image();
+var fg; = new Image();
+var pipeNorth; = new Image();
+var pipeSouth; = new Image();
+
+
+
 
 
 
