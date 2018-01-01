859b1cdccd81586958771c3be21639fe784dd8cd
Rahsaan Basek <rahsaanbasek@gmail.com>
2017-12-31 19:57:33 -0500

    js file draw() images

--- flappyBird.js
+++ flappyBird.js
@@ -1,28 +1,36 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
+
 // load images
 
-var bird; = new Image();
-var bg; = new Image();
-var fg; = new Image();
-var pipeNorth; = new Image();
-var pipeSouth; = new Image();
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
 
-birds.src = "images/bird.png";
+bird.src = "images/bird.png";
 bg.src = "images/bg.png";
 fg.src = "images/fg.png";
 pipeNorth.src = "images/pipeNorth.png";
 pipeSouth.src = "images/pipeSouth.png";
 
 
+// some variables
+
+var gap = 85;
+var constant = pipeNorth.height+gap;
+
 // draw images
 
 function draw(){
 
 	ctx.drawImage(bg, 0, 0);
+    ctx.drawImage(pipeNorth, 100, 0);
+    ctx.drawImage(pipeSouth, 100, 0+constant);
 
-
+    ctx.drawImage(fg,0,cvs.height - fg.height);
 
 }
 
